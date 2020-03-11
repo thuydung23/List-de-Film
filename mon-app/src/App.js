55502b8913d78b2ee757fce76b18492442e46034
thuydung23 <thi.nguyen@insa-cvl.fr>
2020-03-12 00:45:28 +0100

    12/03

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -23,138 +23,119 @@ class App extends Component {
                   <option value={1}>Thriller</option>
                   <option value={2}>Drame</option>
                   <option value={2}>Animation</option>
-                </select>       
-
+                </select>  
                </div>
             </div>
-          <div className="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-            <button type="button" className="btn btn-info btn-block marginB10">Ajouter Film</button>
-          </div>
+          
         </div>
-        <div className="row marginB10">
-          <div className="col-md-offset-7 col-md-5">
-            <form className="form-inline">
-              <div className="form-group">
-                <input type="text" className="form-control" placeholder="Chercher par type de Film" />
-              </div>
-              <div className="form-group">
-                <select className="form-control">
-                  <option value={0}>Comedy</option>
-                  <option value={1}>Thriller</option>
-                  <option value={2}>Drame</option>
-                  <option value={2}>Animation</option>
-                </select>
-              </div>
-              <button type="button" className="btn btn-primary">Valider</button>
-              <button type="button" className="btn btn-default">Annuler</button>
-            </form>
-          </div>
-        </div>
+        
+        <br></br>
         <div className="panel panel-success">
           <div className="panel-heading">List Films</div>
-          <table className="table table-hover ">
-            <thead>
-              <tr>
-                <th style={{width: '10%'}} className="text-center">#</th>
-                <th>Nom</th>
-                <th style={{width: '15%'}} className="text-center">Type</th>
-                <th style={{width: '20%'}}>Like/Dislike</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="text-center">1</td>
-                <td>Oceans 8</td>
-                <td className="text-center"><span className="label label-info">Comedy</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like: 4</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:1</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">2</td>
-                <td>Midnight Sun</td>
-                <td className="text-center"><span className="label label-info">Comedy</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:2</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:0</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">3</td>
-                <td>Les indestructibles 2</td>
-                <td className="text-center"><span className="label label-info">Animation</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:3</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:1</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">4</td>
-                <td>Sans un bruit</td>
-                <td className="text-center"><span className="label label-info">Thriller</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like: 6</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike: 6</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">5</td>
-                <td>Creed II</td>
-                <td className="text-center"><span className="label label-info">Drame</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:16</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:2</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">6</td>
-                <td>Pulp Fiction</td>
-                <td className="text-center"><span className="label label-info">Thriller</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:11</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:3</button>
-                </td>
-              </tr>
-              <tr><td className="text-center">7</td>
-                <td>Pulp Fiction</td>
-                <td className="text-center"><span className="label label-danger">Thriller</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:12333</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:32</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">8</td>
-                <td>Seven</td>
-                <td className="text-center"><span className="label label-info">Thriller</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:11</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:3</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">9</td>
-                <td>Inception</td>
-                <td className="text-center"><span className="label label-info">Thriller</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:2</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:1</button>
-                </td>
-              </tr>
-              <tr>
-                <td className="text-center">10</td>
-                <td>Gone Girl</td>
-                <td className="text-center"><span className="label label-info">Thriller</span></td>
-                <td>
-                  <button type="button" className="btn btn-warning btn-sm">Like:22</button>
-                  <button type="button" className="btn btn-danger btn-sm">Dislike:12</button>
-                  </td>
-                            </tr>
-                        </tbody>
-                    </table>
+		  <div className="items">
+          <div className="row">
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
                 </div>
+              </div>
             </div>
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+              <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <div className="content">
+			  <img src="mon-app/src/anh.jpg" alt="Mountains" style={{width: '100%'}} />
+                <h3>My Work</h3>
+                <div>
+                  <div style={{float: 'left'}}>Like/Dislike : 100/1</div>
+                  <div style={{float: 'left', marginLeft: '10px', color: 'blueviolet', cursor: 'pointer'}}><i className="fas fa-thumbs-up" /></div>
+                  <input type="button" defaultValue="Supprimer" className="btn-remove" style={{float: 'right', cursor: 'pointer'}} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pagination">
+          <a href="#">Précédent</a>
+          <a href="#">1</a>
+          <a href="#" className="active">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">6</a>
+          <a href="#">Suivant</a>
+        </div>
+      </div>
+	  </div>
         );
     }
 }
@@ -162,3 +143,4 @@ class App extends Component {
 export default App;
 
     
+
